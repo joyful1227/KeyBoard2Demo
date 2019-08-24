7d32f50b58c685d39867459515c64c8a94eab579
joy <le821227@gmail.com>
2019-08-25 02:14:25 +0800

    version1.0

--- KeyBoard2Demo/ViewController.swift
+++ KeyBoard2Demo/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var accountTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
+    
 }
+extension ViewController: UITextFieldDelegate {
+    
+    
+    //當點擊view任何喔一處鍵盤收起
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 
+    //按鍵盤上的return鍵可以換
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       let nextTag = textField.tag+1
+        
+        if let nextResponder = textField.superview?.viewWithTag(nextTag) {
+            nextResponder.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+//        if textField == accountTextField {
+//           passwordTextField.becomeFirstResponder()
+//        }else {
+//            textField.endEditing(true)
+//        }
+        
+        return true
+    }
+}
